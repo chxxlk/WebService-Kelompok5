b55737b414e635a047b78ed0ca4507882c8a8832
chxxlk <chrislekpey06@gmail.com>
2026-07-22 07:32:02 +0700

    lebih sederhana

--- src/main/java/com/kelompok5/orchestrator/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kelompok5.orchestrator.repository;
-
-import com.kelompok5.orchestrator.model.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderRepository extends MongoRepository<Order, String> {
-}
